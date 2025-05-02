4fd11a77417045b7d140bf1a6410e4daaddd5632
Reid Mosieur <rmosieur@gmail.com>
2025-05-02 11:16:51 -0400

    rebrand standard user

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -20,52 +20,53 @@ import Link from "next/link";
 import { appName } from "@/data/app";
 
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Dashboard",
-      url: "/",
-      icon: IconDashboard,
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Settings",
-      url: "/settings",
-      icon: SettingsIcon,
-    },
-  ],
-};
+	user: {
+		name: 'reid',
+		email: 'rmosieur@gmail.com',
+		avatar: '/avatars/reid.jpg',
+		fallback: 'RM',
+	},
+	navMain: [
+		{
+			title: 'Dashboard',
+			url: '/',
+			icon: IconDashboard,
+		},
+	],
+	navSecondary: [
+		{
+			title: 'Settings',
+			url: '/settings',
+			icon: SettingsIcon,
+		},
+	],
+}
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="offcanvas" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
-            >
-              <Link href="#">
-                <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">{appName}</span>
-              </Link>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={data.user} />
-      </SidebarFooter>
-    </Sidebar>
-  );
+		<Sidebar collapsible="offcanvas" {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton
+							asChild
+							className="data-[slot=sidebar-menu-button]:!p-1.5"
+						>
+							<Link href="#" className="text-foreground">
+								<IconInnerShadowTop className="!size-5" />
+								<span className="text-base font-semibold">{appName}</span>
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<NavMain items={data.navMain} />
+				<NavSecondary items={data.navSecondary} className="mt-auto" />
+			</SidebarContent>
+			<SidebarFooter>
+				<NavUser user={data.user} />
+			</SidebarFooter>
+		</Sidebar>
+	)
 }
