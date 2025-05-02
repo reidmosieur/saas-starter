47abe13ff9801c083e92bf66b29991832f34ad5f
Reid Mosieur <rmosieur@gmail.com>
2025-05-02 10:42:52 -0400

    added contact and location information

--- src/data/app.ts
+++ src/data/app.ts
@@ -1,1 +1,14 @@
-export const appName = "Acme Inc.";
+export const appName = 'Acme Inc.'
+export const contact = {
+	email: 'support@example.com',
+}
+export const location = {
+	addressLine1: '123 E Main Street',
+	addressLine2: 'Suite 101',
+	city: 'New York',
+	state: 'New York',
+	county: 'New York County',
+	zip: '12345',
+	country: 'United States',
+	countryCode: 'US',
+}
