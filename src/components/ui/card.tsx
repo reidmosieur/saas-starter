8c7633cea53cb24350962aadfdfb7aed16dde4f1
Reid Mosieur <rmosieur@gmail.com>
2025-05-02 15:57:55 -0400

    slotable card for card forms

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,18 +1,26 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { Slot } from '@radix-ui/react-slot'
 
-function Card({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card"
-      className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
-        className
-      )}
-      {...props}
-    />
-  )
+function Card({
+	className,
+	asChild,
+	...props
+}: React.ComponentProps<'div'> & {
+	asChild?: boolean
+}) {
+	const Comp = asChild ? Slot : 'div'
+	return (
+		<Comp
+			data-slot="card"
+			className={cn(
+				'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm',
+				className,
+			)}
+			{...props}
+		/>
+	)
 }
 
 function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
