d9a44068df1a0f1fc320b6b18f2227ffb4ec112a
Reid Mosieur <rmosieur@gmail.com>
2025-05-02 14:40:57 -0400

    organization

---
+++ src/components/account/settings.tsx
@@ -0,0 +1,201 @@
+'use client'
+
+import { Form } from '@/components/ui/form'
+import {
+	firstNameSchema,
+	lastNameSchema,
+	phoneNumberSchema,
+	usernameSchema,
+} from '@/schema/account'
+import { emailSchema } from '@/schema/auth'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { AvatarFallback } from '@radix-ui/react-avatar'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { EmailField } from '../auth/fields'
+import { Avatar, AvatarImage } from '../ui/avatar'
+import { Button } from '../ui/button'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '../ui/card'
+import { NameField, PhoneNumberField, UsernameField } from './fields'
+import { CardFormProps } from '@/types'
+
+const accountSettingsForm = z.object({
+	username: usernameSchema,
+	firstName: firstNameSchema,
+	lastName: lastNameSchema,
+})
+
+export function AccountSettingsForm({ cardProps }: CardFormProps) {
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof accountSettingsForm>>({
+		resolver: zodResolver(accountSettingsForm),
+		defaultValues: {
+			username: '',
+		},
+	})
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof accountSettingsForm>) {
+		// placeholder
+		console.log(values)
+	}
+
+	return (
+		<Form {...form}>
+			<Card {...cardProps} asChild>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<CardHeader>
+						<CardTitle>Account</CardTitle>
+						<CardDescription>
+							Change your name, username, and other information
+						</CardDescription>
+					</CardHeader>
+					<CardContent className="grid gap-4 md:gap-6">
+						<NameField form={form} />
+						<UsernameField form={form} />
+					</CardContent>
+					<CardFooter className="mt-auto justify-end">
+						<Button>Save</Button>
+					</CardFooter>
+				</form>
+			</Card>
+		</Form>
+	)
+}
+
+const emailSettingsForm = z.object({
+	email: emailSchema,
+})
+
+export function EmailSettingsForm({ cardProps }: CardFormProps) {
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof emailSettingsForm>>({
+		resolver: zodResolver(emailSettingsForm),
+		defaultValues: {
+			email: '',
+		},
+	})
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof emailSettingsForm>) {
+		// placeholder
+		console.log(values)
+	}
+
+	return (
+		<Form {...form}>
+			<Card {...cardProps} asChild>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<CardHeader>
+						<CardTitle>Email</CardTitle>
+						<CardDescription>
+							Enter the email addresses you want associated with your account
+						</CardDescription>
+					</CardHeader>
+					<CardContent className="grid gap-4 md:gap-6">
+						<EmailField form={form} />
+					</CardContent>
+					<CardFooter className="mt-auto justify-end">
+						<Button>Save</Button>
+					</CardFooter>
+				</form>
+			</Card>
+		</Form>
+	)
+}
+
+const phoneNumberSettingsForm = z.object({
+	phoneNumber: phoneNumberSchema,
+})
+
+export function PhonenumberSettingsForm({ cardProps }: CardFormProps) {
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof phoneNumberSettingsForm>>({
+		resolver: zodResolver(phoneNumberSettingsForm),
+		defaultValues: {
+			phoneNumber: {
+				countryCode: 'US',
+				number: '',
+			},
+		},
+	})
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof phoneNumberSettingsForm>) {
+		// placeholder
+		console.log(values)
+	}
+	return (
+		<Form {...form}>
+			<Card {...cardProps} asChild>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<CardHeader>
+						<CardTitle>Your Phone Number</CardTitle>
+						<CardDescription>
+							Enter your phone number to receive important updates regarding
+							your account
+						</CardDescription>
+					</CardHeader>
+					<CardContent className="grid gap-4 md:gap-6">
+						<PhoneNumberField form={form} />
+					</CardContent>
+					<CardFooter className="mt-auto justify-end">
+						<Button>Save</Button>
+					</CardFooter>
+				</form>
+			</Card>
+		</Form>
+	)
+}
+
+export function AvatarSettingsForm({ cardProps }: CardFormProps) {
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof phoneNumberSettingsForm>>({
+		resolver: zodResolver(phoneNumberSettingsForm),
+		defaultValues: {
+			phoneNumber: {
+				countryCode: 'US',
+				number: '',
+			},
+		},
+	})
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof phoneNumberSettingsForm>) {
+		// placeholder
+		console.log(values)
+	}
+	return (
+		<Form {...form}>
+			<Card {...cardProps} asChild>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<CardHeader>
+						<CardTitle>Avatar</CardTitle>
+						<CardDescription>
+							This is your avatar. Click on the avatar to upload a custom one.
+						</CardDescription>
+					</CardHeader>
+					<CardContent className="h-full">
+						<Avatar className="h-full w-full">
+							<AvatarImage
+								className="aspect-square h-full w-full"
+								src={'https://placehold.co/600x600/EEE/31343C'}
+							/>
+							<AvatarFallback>RM</AvatarFallback>
+						</Avatar>
+					</CardContent>
+					<CardFooter className="mt-auto justify-end">
+						<Button>Save</Button>
+					</CardFooter>
+				</form>
+			</Card>
+		</Form>
+	)
+}
---
+++ src/components/auth/settings.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import { Form } from '@/components/ui/form'
+import { passwordSchema } from '@/schema/auth'
+import { CardFormProps } from '@/types'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { PasswordField } from '../auth/fields'
+import { LabeledInputField } from '../fields'
+import { Button } from '../ui/button'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '../ui/card'
+import { IconBrandApple, IconBrandGoogle } from '@tabler/icons-react'
+import { EllipsisVertical } from 'lucide-react'
+
+const emailSettingsForm = z.object({
+	password: passwordSchema,
+	verifyPassword: passwordSchema,
+})
+
+export function PasswordSettingsForm({ cardProps }: CardFormProps) {
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof emailSettingsForm>>({
+		resolver: zodResolver(emailSettingsForm),
+		defaultValues: {
+			password: '',
+			verifyPassword: '',
+		},
+	})
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof emailSettingsForm>) {
+		// placeholder
+		console.log(values)
+	}
+
+	return (
+		<Form {...form}>
+			<Card {...cardProps} asChild>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<CardHeader>
+						<CardTitle>Password</CardTitle>
+						<CardDescription>Change your password</CardDescription>
+					</CardHeader>
+					<CardContent className="grid gap-4 md:gap-6">
+						<PasswordField form={form} />
+						<LabeledInputField
+							form={form}
+							name={'verifyPassword'}
+							label="Verify Password"
+						/>
+					</CardContent>
+					<CardFooter className="mt-auto justify-end">
+						<Button>Save</Button>
+					</CardFooter>
+				</form>
+			</Card>
+		</Form>
+	)
+}
+
+const oauthSettingsForm = z.object({
+	password: passwordSchema,
+	verifyPassword: passwordSchema,
+})
+
+export function OAuthSettingsForm({ cardProps }: CardFormProps) {
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof oauthSettingsForm>>({
+		resolver: zodResolver(oauthSettingsForm),
+		defaultValues: {
+			password: '',
+			verifyPassword: '',
+		},
+	})
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof oauthSettingsForm>) {
+		// placeholder
+		console.log(values)
+	}
+
+	return (
+		<Form {...form}>
+			<Card {...cardProps} asChild>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<CardHeader>
+						<CardTitle>SSO Methods</CardTitle>
+						<CardDescription>
+							Change SSO providers, like Google or Apple
+						</CardDescription>
+					</CardHeader>
+					<CardContent className="grid gap-4 md:gap-6">
+						<div className="flex items-center justify-between gap-4 md:gap-8">
+							<div className="flex items-center gap-2">
+								<IconBrandGoogle className="h-10 w-10" />
+								<div className="flex flex-col">
+									<span className="font-bold">Google</span>
+									<small className="text-muted-foreground">username</small>
+								</div>
+							</div>
+							<Button variant={'ghost'}>
+								<EllipsisVertical />
+							</Button>
+						</div>
+						<div className="flex items-center justify-between gap-4 md:gap-8">
+							<div className="flex items-center gap-2">
+								<IconBrandApple className="h-10 w-10" />
+								<div className="flex flex-col">
+									<span className="font-bold">Apple</span>
+									<small className="text-muted-foreground">username</small>
+								</div>
+							</div>
+							<Button variant={'ghost'}>
+								<EllipsisVertical />
+							</Button>
+						</div>
+					</CardContent>
+				</form>
+			</Card>
+		</Form>
+	)
+}
