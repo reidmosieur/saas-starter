80d48389070cbe53cc428cc636f73b918b908809
Reid Mosieur <rmosieur@gmail.com>
2025-05-02 15:57:26 -0400

    settings wireframe

---
+++ src/components/settings-tabs.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { ReactNode } from 'react'
+
+export function SettingsTabs({ children }: { children?: ReactNode }) {
+	const pathname = usePathname()
+	const scrubbedPathname = pathname.replace('/settings', '').replaceAll('/', '')
+	const defaultValue = scrubbedPathname === '' ? 'account' : scrubbedPathname
+	return (
+		// the key is critical to allow the Tabs to rerender when the user navigates outside of the TabsList
+		// eg: in breadcrumbs
+		<Tabs key={pathname} defaultValue={defaultValue}>
+			<TabsList className="w-full">
+				<TabsTrigger value="account" asChild>
+					<Link href={'/settings'}>General</Link>
+				</TabsTrigger>
+				<TabsTrigger value="security" asChild>
+					<Link href={'/settings/security'}>Security & Privacy</Link>
+				</TabsTrigger>
+				<TabsTrigger value="billing" asChild>
+					<Link href={'/settings/billing'}>Billling</Link>
+				</TabsTrigger>
+				<TabsTrigger value="invoices" asChild>
+					<Link href={'/settings/invoices'}>Invoices</Link>
+				</TabsTrigger>
+			</TabsList>
+			{children}
+		</Tabs>
+	)
+}
