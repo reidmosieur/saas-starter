865368029639f2655c4819f9cd2a6cb409620306
Reid Mosieur <rmosieur@gmail.com>
2025-05-05 08:18:34 -0400

    better form error messages for UX

--- src/schema/account.ts
+++ src/schema/account.ts
@@ -1,14 +1,24 @@
 import { z } from 'zod'
 
-const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/
 export const usernameSchema = z
 	.string()
-	.min(4)
-	.max(16)
-	.superRefine((value) => usernameRegex.test(value))
+	.min(4, { message: `Please provide a username that's at least 4 characters` })
+	.max(16, {
+		message: `Please provide a username that's at least 16 characters`,
+	})
+	.regex(/^[a-zA-Z0-9_]{3,20}$/, {
+		message: `Please only use letters, numbers, and underscores`,
+	})
+	.trim()
 
-export const firstNameSchema = z.string()
-export const lastNameSchema = z.string()
+export const firstNameSchema = z
+	.string()
+	.min(2, { message: `Please provide a name that's at least 2 characters` })
+	.trim()
+export const lastNameSchema = z
+	.string()
+	.min(2, { message: `Please provide a name that's at least 2 characters` })
+	.trim()
 
 export const phoneNumberSchema = z.object({
 	countryCode: z.string(), // todo: enum for country codes
--- src/schema/auth.ts
+++ src/schema/auth.ts
@@ -1,5 +1,31 @@
 import { z } from 'zod'
 
-export const passwordSchema = z.string().min(8)
+export const passwordSchema = z
+	.string()
+	.min(8, {
+		message: `Please provide a password that's at least 8 characters long`,
+	})
+	.regex(/[a-zA-Z]/, {
+		message: `Please provide a password that contains at least one letter.`,
+	})
+	.regex(/[0-9]/, {
+		message: 'Please provide a password that contains at least one number.',
+	})
+	.regex(/[^a-zA-Z0-9]/, {
+		message:
+			'Please provide a password that contains at least one special character.',
+	})
 
-export const emailSchema = z.string().email()
+export const emailSchema = z
+	.string()
+	.email({ message: 'Please provide a valid email' })
+
+export const emailSignUpSchema = z.object({
+	email: emailSchema,
+	password: passwordSchema,
+})
+
+export const emailLoginSchema = z.object({
+	email: emailSchema,
+	password: z.string({ message: 'Please provide a password' }),
+})
