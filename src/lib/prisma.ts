690a75fde5861cc3ffa67781e6d768585065f53b
Reid Mosieur <rmosieur@gmail.com>
2025-05-05 14:41:45 -0400

    extend prisma to have exists for auth

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,30 @@
-import { PrismaClient } from '@/generated/prisma'
+import { Prisma, PrismaClient } from '@/generated/prisma'
 
 const globalForPrisma = global as unknown as {
 	prisma: PrismaClient
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma =
+	globalForPrisma.prisma ||
+	new PrismaClient().$extends({
+		model: {
+			$allModels: {
+				// source: https://github.com/prisma/prisma-client-extensions/blob/main/exists-fn/script.ts
+				async exists<T>(
+					this: T,
+					where: Prisma.Args<T, 'findFirst'>['where'],
+				): Promise<boolean> {
+					const context = Prisma.getExtensionContext(this)
+					// eslint-disable-next-line @typescript-eslint/no-explicit-any
+					const result = await (context as any).findFirst({
+						where,
+						select: { id: true },
+					})
+					return result !== null
+				},
+			},
+		},
+	})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
