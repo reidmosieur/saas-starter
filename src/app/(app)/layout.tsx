81eac5a2eb52e49478e3480a388cf3193061a161
Reid Mosieur <rmosieur@gmail.com>
2025-05-07 10:07:00 -0400

    added user information to layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,26 +1,30 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { SiteHeader } from "@/components/site-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from '@/components/app-sidebar'
+import { NavUser } from '@/components/nav-user'
+import { SiteHeader } from '@/components/site-header'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { handleUserInitialization } from '../actions/user'
 
-export default function Layout({
-  children,
+export default async function Layout({
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        {children}
-      </SidebarInset>
-    </SidebarProvider>
-  );
+	const user = await handleUserInitialization()
+
+	return (
+		<SidebarProvider
+			style={
+				{
+					'--sidebar-width': 'calc(var(--spacing) * 72)',
+					'--header-height': 'calc(var(--spacing) * 12)',
+				} as React.CSSProperties
+			}
+		>
+			<AppSidebar variant="inset" navUser={<NavUser {...user} />} />
+			<SidebarInset>
+				<SiteHeader />
+				{children}
+			</SidebarInset>
+		</SidebarProvider>
+	)
 }
