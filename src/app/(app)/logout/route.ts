94216f4c158fa0f403dcfcc3189f8d7fd159290d
Reid Mosieur <rmosieur@gmail.com>
2025-05-12 16:18:46 -0400

    better routing

--- src/app/(app)/logout/route.ts
+++ src/app/(app)/logout/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server'
 import { handleLogout } from '@/app/actions/auth/logout'
+import { loginRoute } from '@/constants/routes'
 
 export async function GET() {
 	await handleLogout()
-	return NextResponse.redirect('/login')
+	return NextResponse.redirect(loginRoute)
 }
