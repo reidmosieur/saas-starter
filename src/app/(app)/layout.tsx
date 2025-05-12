84d2238e85851d81a01337fba1743a57812cb6d4
Reid Mosieur <rmosieur@gmail.com>
2025-05-12 16:17:19 -0400

    organization context set up

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,6 +4,7 @@ import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { UserContextProvider } from '@/context/user'
 import { handleUserInitialization } from '../actions/user'
 import { constructNavigation } from '@/lib/access-control'
+import { OrganizationContextProvider } from '@/context/organization'
 
 export default async function Layout({
 	children,
@@ -15,20 +16,22 @@ export default async function Layout({
 
 	return (
 		<UserContextProvider defaultValues={user}>
-			<SidebarProvider
-				style={
-					{
-						'--sidebar-width': 'calc(var(--spacing) * 72)',
-						'--header-height': 'calc(var(--spacing) * 12)',
-					} as React.CSSProperties
-				}
-			>
-				<AppSidebar variant="inset" permittedRoutes={permittedRoutes} />
-				<SidebarInset>
-					<SiteHeader />
-					{children}
-				</SidebarInset>
-			</SidebarProvider>
+			<OrganizationContextProvider defaultValues={user.organization}>
+				<SidebarProvider
+					style={
+						{
+							'--sidebar-width': 'calc(var(--spacing) * 72)',
+							'--header-height': 'calc(var(--spacing) * 12)',
+						} as React.CSSProperties
+					}
+				>
+					<AppSidebar variant="inset" permittedRoutes={permittedRoutes} />
+					<SidebarInset>
+						<SiteHeader />
+						{children}
+					</SidebarInset>
+				</SidebarProvider>
+			</OrganizationContextProvider>
 		</UserContextProvider>
 	)
 }
