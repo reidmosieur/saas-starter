2e7d02b30be39b8711b4dac96a668c5933c8e92b
Reid Mosieur <rmosieur@gmail.com>
2025-05-12 16:15:47 -0400

    phone number combobox fields set up

--- src/components/account/fields.tsx
+++ src/components/account/fields.tsx
@@ -1,6 +1,28 @@
+'use client'
+
+import { Check, ChevronDown } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+} from '@/components/ui/command'
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+import countries from 'i18n-iso-countries'
+import countriesLocale from 'i18n-iso-countries/langs/en.json'
+import { ComponentProps } from 'react'
 import { FieldValues, Path, UseFormReturn } from 'react-hook-form'
 import { FieldProps, LabeledInputField } from '../fields'
-import { ComponentProps } from 'react'
+import { FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
 
 type UsernameFieldProps<T extends FieldValues & { username: string }> = {
 	form: UseFormReturn<T>
@@ -103,5 +125,105 @@ export function PhoneNumberField<
 				labelProps={{ children: 'Phone Number' }}
 			/>
 		</div>
+	)
+}
+
+countries.registerLocale(countriesLocale)
+export function CountriesCombobox<
+	T extends { countryCode: string; phoneNumber: string },
+>({ form }: PhoneNumberProps<T>) {
+	const countryCodesObject = countries.getAlpha2Codes()
+	const countryCodes = Object.entries(countryCodesObject)
+
+	return (
+		<FormField
+			control={form.control}
+			name={'countryCode' as Path<T>}
+			render={({ field }) => (
+				<FormItem>
+					<Popover>
+						<PopoverTrigger asChild>
+							<Button
+								variant="ghost"
+								role="combobox"
+								className="w-fit justify-between"
+							>
+								<span
+									className={cn(
+										'fi',
+										`fi-${field.value ? field.value.toLowerCase() : 'us'}`,
+									)}
+								></span>
+								{field.value}
+								<ChevronDown />
+							</Button>
+						</PopoverTrigger>
+						<PopoverContent className="w-fit p-0" align="start">
+							<Command>
+								<CommandInput placeholder="Country" />
+								<CommandList>
+									<CommandEmpty>Country not found.</CommandEmpty>
+									<CommandGroup>
+										{countryCodes.map(([code]) => (
+											<CommandItem
+												key={code}
+												value={code}
+												onSelect={(currentValue) => {
+													form.setValue('countryCode' as Path<T>, currentValue)
+												}}
+											>
+												<Check
+													className={cn(
+														'mr-2 h-4 w-4',
+														field.value === code ? 'opacity-100' : 'opacity-0',
+													)}
+												/>
+												<span
+													className={cn('fi', `fi-${code.toLowerCase()}`)}
+												></span>
+												<span>{code}</span>{' '}
+												<span>{countries.getName(code, 'en')}</span>
+											</CommandItem>
+										))}
+									</CommandGroup>
+								</CommandList>
+							</Command>
+						</PopoverContent>
+					</Popover>
+				</FormItem>
+			)}
+		/>
+	)
+}
+
+export function PhoneNumberFlagField<
+	T extends { countryCode: string; phoneNumber: string },
+>({ form }: PhoneNumberProps<T>) {
+	return (
+		<FormItem>
+			<FormLabel>Phone number</FormLabel>
+			<div className="flex gap-2 rounded-md border shadow">
+				<CountriesCombobox form={form} />
+				<FormField
+					control={form.control}
+					name={'phoneNumber' as Path<T>}
+					render={({ field }) => (
+						<FormItem>
+							<input className="w-full focus:outline-none" {...field} />
+						</FormItem>
+					)}
+				/>
+			</div>
+			{form.getFieldState('countryCode' as Path<T>).error ? (
+				<FormMessage>
+					{form.getFieldState('countryCode' as Path<T>).error?.message}
+				</FormMessage>
+			) : null}
+			{form.getFieldState('phoneNumber' as Path<T>).error ? (
+				<FormMessage>
+					{form.getFieldState('phoneNumber' as Path<T>).error?.message}
+				</FormMessage>
+			) : null}
+		</FormItem>
 	)
 }
