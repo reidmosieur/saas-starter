0ec848cc7dc088ba21f3583265a6e5f9fd8e4889
Reid Mosieur <rmosieur@gmail.com>
2025-05-16 13:16:56 -0400

    setup dashboard

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return null;
+import { ChartAreaInteractive } from '@/components/dashboard-demo/chart-area-interactive'
+import { DataTable } from '@/components/dashboard-demo/data-table'
+import { SectionCards } from '@/components/dashboard-demo/section-cards'
+import data from '@/data/dashboard.json'
+
+export default async function Home() {
+	return (
+		<div className="flex flex-1 flex-col">
+			<div className="@container/main flex flex-1 flex-col gap-2">
+				<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+					<SectionCards />
+					<div className="px-4 lg:px-6">
+						<ChartAreaInteractive />
+					</div>
+					<DataTable data={data} />
+				</div>
+			</div>
+		</div>
+	)
 }
